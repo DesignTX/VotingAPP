e507d8deb00cf2321c0b6c462bf765f505ee6c36
Tony Xu <Design.TXU@gmail.com>
2019-06-19 11:28:45 +1000

    Login and Registration complete, need to add credentials

--- serverside/models/index.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.set("debug", true); //debugger information sent to term
-mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost/voting", { useNewUrlParser: true });
-
-module.exports.User = require("./user");
-module.exports.Poll = require("./polls");
